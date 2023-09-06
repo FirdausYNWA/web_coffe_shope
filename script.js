e9443a9572c688cf7703844c7a65e6eec8b58fcf
FirdausYNWA <firdausynwa5@gmail.com>
2023-09-06 21:39:40 +0700

    add file yg sudah dibuat

---
+++ script.js
@@ -0,0 +1,9 @@
+// toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+//ketika humberger di klik menu muncul
+document.querySelector('#humberger-menu').
+onclick = ()=>{
+    navbarNav.classList.toggle('active');
+}
